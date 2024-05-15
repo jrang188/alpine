22ac1f92485c9f25d51ef79687b63db6418b074e
Justin Ang <59113087+jrang188@users.noreply.github.com>
2024-05-16 02:10:00 +0700

    update footer

--- app.config.ts
+++ app.config.ts
@@ -19,8 +19,13 @@ export default defineAppConfig({
     },
     footer: {
       credits: {
-        enabled: true, // possible value are : true | false
-        repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
+        // possible value are : true | false
+        enabled: true,
+
+        // our github repository
+        repository: 'https://github.com/jrang188',
+
+        text: 'Justin Ang'
       },
       navigation: true, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
@@ -37,6 +42,18 @@ export default defineAppConfig({
     },
     form: {
       successMessage: 'Message sent. Thank you!'
+    }
+  },
+
+  prose: {
+    h1: {
+      icon: 'key-116'
+    }
+  },
+
+  nuxtIcon: {
+    aliases: {
+      'key-116': ''
     }
   }
 })
