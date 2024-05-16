fbabc8345fcca5bcdb4319339be04cdb06e940d9
Justin Ang <jrang@sfu.ca>
2024-05-16 23:03:53 +0700

    chore: Update alpine configuration for Justin Ang's portfolio website

--- app.config.ts
+++ app.config.ts
@@ -1,21 +1,21 @@
 // https://github.com/nuxt-themes/alpine/blob/main/nuxt.schema.ts
 export default defineAppConfig({
   alpine: {
-    title: 'Justin Ang',
-    description: 'Portfolio Website of Justin Ang powered by Nuxt Content',
+    title: "Justin Ang",
+    description: "Portfolio Website of Justin Ang powered by Nuxt Content",
     image: {
-      src: '/personalpic.jpeg',
-      alt: 'Justin Ang',
+      src: "/personalpic.jpeg",
+      alt: "Justin Ang",
       width: 400,
-      height: 300
+      height: 300,
     },
     header: {
-      position: 'left', // possible value are : | 'left' | 'center' | 'right'
+      position: "left", // possible value are : | 'left' | 'center' | 'right'
       logo: {
-        path: '/light_logo.png', // path of the logo
-        pathDark: '/dark_logo.png', // path of the logo in dark mode, leave this empty if you want to use the same logo
-        alt: 'Justin' // alt of the logo
-      }
+        path: "/light_logo.png", // path of the logo
+        pathDark: "/dark_logo.png", // path of the logo in dark mode, leave this empty if you want to use the same logo
+        alt: "Justin", // alt of the logo
+      },
     },
     footer: {
       credits: {
@@ -23,40 +23,42 @@ export default defineAppConfig({
         enabled: true,
 
         // our github repository
-        repository: 'https://github.com/jrang188',
+        repository: "https://github.com/jrang188",
 
-        text: 'Justin Ang'
+        text: "Justin Ang",
       },
       navigation: true, // possible value are : true | false
-      alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
-      message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
+      alignment: "center", // possible value are : 'none' | 'left' | 'center' | 'right'
+      message: "Follow me on", // string that will be displayed in the footer (leave empty or delete to disable)
     },
     socials: {
-      twitter: '',
-      instagram: '',
+      twitter: "",
+      instagram: "",
 
       linkedin: {
-        icon: 'uil:linkedin',
-        label: 'LinkedIn',
-        href: 'https://www.linkedin.com/in/jrang188/'
+        icon: "uil:linkedin",
+        label: "LinkedIn",
+        href: "https://www.linkedin.com/in/jrang188/",
       },
 
-      github: 'https://github.com/jrang188'
+      github: "https://github.com/jrang188",
     },
     form: {
-      successMessage: 'Message sent. Thank you!'
-    }
+      successMessage: "Message sent. Thank you!",
+    },
   },
-
+  head: {
+    link: [{ rel: "icon", type: "image/png", href: "favicon.ico" }],
+  },
   prose: {
     h1: {
-      icon: ''
-    }
+      icon: "",
+    },
   },
 
   nuxtIcon: {
     aliases: {
-      'key-116': ''
-    }
-  }
-})
+      "key-116": "",
+    },
+  },
+});
