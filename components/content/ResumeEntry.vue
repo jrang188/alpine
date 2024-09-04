827bd16df1bbc904f44dd308dfd3bcd492504000
Justin Ang <jrang@sfu.ca>
2024-09-04 13:58:39 -0700

    Update job title in ResumeEntry component

--- components/content/ResumeEntry.vue
+++ components/content/ResumeEntry.vue
@@ -20,24 +20,41 @@
 
 <template>
   <div class="resume-entry">
-    <h4 class="title">
+    <div class="title">
       <ContentSlot :use="$slots.title" unwrap="p"> Job Title </ContentSlot>
-    </h4>
+    </div>
     <div>
       <ContentSlot :use="$slots.company" unwrap="p"> Company Name </ContentSlot>
     </div>
     <div>
-      <div><ContentSlot :use="$slots.startDate" unwrap="p">Start Date</ContentSlot> - <ContentSlot :use="$slots.endDate" unwrap="p">End Date</ContentSlot></div>
-      <div><ContentSlot :use="$slots.location" unwrap="p">Company Location</ContentSlot> </div>
+      <div>
+        <ContentSlot :use="$slots.startDate" unwrap="p">Start Date</ContentSlot>
+        - <ContentSlot :use="$slots.endDate" unwrap="p">End Date</ContentSlot>
+      </div>
+      <div>
+        <ContentSlot :use="$slots.location" unwrap="p">
+          Company Location
+        </ContentSlot>
+      </div>
     </div>
-    <ul>
-      <ContentSlot :use="$slots.bulletPoints" unwrap="ul"> 
+    <ContentSlot :use="$slots.bulletPoints" unwrap="ul li">
+      <ul>
         <li>Bullet Point 1</li>
         <li>Bullet Point 2</li>
-        <li>Bullet Point 3</li>  
-      </ContentSlot>
-    </ul>
+        <li>Bullet Point 3</li>
+      </ul>
+    </ContentSlot>
   </div>
 </template>
 
-<style></style>
+<style scoped lang="ts">
+css({
+ '.resume-entry': {
+    '.title': {
+      fontSize: '{text.2xl.fontSize}',
+      lineHeight: '{text.2xl.lineHeight}',
+      fontWeight: '{fontWeight.bold}',
+    },
+  }
+})
+</style>
