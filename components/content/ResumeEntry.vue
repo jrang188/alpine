08ab8a55d3b8a29e0c9309b3a193b2b51de89468
Justin Ang <jrang@sfu.ca>
2024-09-04 13:04:31 -0700

    resumeEntry

---
+++ components/content/ResumeEntry.vue
@@ -0,0 +1,43 @@
+<script lang="ts" setup>
+// import type { PropType } from 'vue'
+
+// interface ResumeEntry {
+//   jobTitle: string
+//   companyName: string
+//   // startDate: Date
+//   // endDate: Date
+//   // location: string
+//   // bulletPoints: string[]
+// }
+
+// defineProps({
+//   resumeEntry: {
+//     type: Object as PropType<ResumeEntry>,
+//     required: true,
+//   },
+// });
+</script>
+
+<template>
+  <div class="resume-entry">
+    <h4 class="title">
+      <ContentSlot :use="$slots.title" unwrap="p"> Job Title </ContentSlot>
+    </h4>
+    <div>
+      <ContentSlot :use="$slots.company" unwrap="p"> Company Name </ContentSlot>
+    </div>
+    <div>
+      <div><ContentSlot :use="$slots.startDate" unwrap="p">Start Date</ContentSlot> - <ContentSlot :use="$slots.endDate" unwrap="p">End Date</ContentSlot></div>
+      <div><ContentSlot :use="$slots.location" unwrap="p">Company Location</ContentSlot> </div>
+    </div>
+    <ul>
+      <ContentSlot :use="$slots.bulletPoints" unwrap="ul"> 
+        <li>Bullet Point 1</li>
+        <li>Bullet Point 2</li>
+        <li>Bullet Point 3</li>  
+      </ContentSlot>
+    </ul>
+  </div>
+</template>
+
+<style></style>
